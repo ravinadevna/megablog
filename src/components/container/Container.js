c939451bba7ad3a3ae8f0e3395b0d4f37f230313
ravinadevna <ravinadevna@gmail.com>
2023-10-28 21:50:02 +0530

    react code

---
+++ src/components/container/Container.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Container({children}){
+    return(
+        <div className="w-full max-w-7xl mx-aut0
+        px-4">{children}</div>
+    )
+}
+
+
+export default Container;
